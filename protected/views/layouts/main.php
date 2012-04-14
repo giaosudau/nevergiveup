57fa6ec6c32433fb8d39acfa50f3b048152df2ea
AT Phan <atphanqt@gmail.com>
2012-04-14 09:41:38 +0700

    Thang commit

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,7 +29,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-                                array('label'=>'Profile', 'url'=>array('/profile.index')),
+                                array('label'=>'Profile', 'url'=>array('/profile/index'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
